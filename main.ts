11aba938fd4fb0a29cb914aa606aa6face411ccc
Jack149 <31501037+Jack149@users.noreply.github.com>
2021-12-06 22:45:25 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+radio.onReceivedNumber(function (receivedNumber) {
+	
+})
+radio.onReceivedString(function (receivedString) {
+	
+})
+radio.onReceivedValue(function (name, value) {
+	
+})
 input.setAccelerometerRange(AcceleratorRange.OneG)
 basic.forever(function () {
     if (true) {
